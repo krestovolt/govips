abdc64fcfdcb9e7c491cc2afea65a3de1ba541a5
Kautsar A B <kautsar.ab@gmail.com>
2023-07-21 14:23:08 +0700

    Add nil check when checking the first 20-bytes

--- vips/foreign.go
+++ vips/foreign.go
@@ -157,6 +157,10 @@ func IsTypeSupported(imageType ImageType) bool {
 func DetermineImageTypeFromSource(source *Source) ImageType {
 	length := 20
 	data := C.source_sniff(source.vipsSrc, C.ulong(length))
+	if data == nil {
+		return ImageTypeUnknown
+	}
+
 	sh := &reflect.SliceHeader{
 		Data: uintptr(data),
 		Len:  length,
